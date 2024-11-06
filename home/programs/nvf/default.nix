92c3259133c698b7ec2aef8671779e9c286bbc8c
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2024-11-06 07:03:40 -0300

    Update nvf config

--- home/programs/nvf/default.nix
+++ home/programs/nvf/default.nix
@@ -3,7 +3,17 @@
   imports = [ inputs.nvf.homeManagerModules.default ];
   programs.nvf = {
     enable = true;
+    enableManpages = true;
     settings = {
+      vim.visuals = {
+        enable = true;
+        cursorline.enable = true;
+      };
+      vim.theme = {
+        enable = true;
+        name = "catppuccin";
+        style = "mocha";
+      };
       vim.lsp = {
         enable = true;
       };
