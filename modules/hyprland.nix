f6699b07ac37e3e8377a0944477a1816773f9a2e
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2025-01-03 18:14:49 -0300

    Update hyprland module

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -13,6 +13,7 @@
       sddm = {
         wayland.enable = true;
         enable = true;
+        package = pkgs.kdePackages.sddm;
       };
     };
   };
@@ -21,9 +22,20 @@
     enable = true;
   };
 
+  catppuccin = {
+    flavor = "mocha";
+    accent = "blue";
+    sddm = {
+      enable = true;
+      font = "Lexend";
+      fontSize = "12";
+    };
+    grub.enable = true;
+  };
+
   programs.ssh.startAgent = true;
-  programs.thunar.enable = true;
   programs.xfconf.enable = true;
+  programs.thunar.enable = true;
   programs.thunar.plugins = with pkgs.xfce; [
     thunar-archive-plugin
     thunar-volman
