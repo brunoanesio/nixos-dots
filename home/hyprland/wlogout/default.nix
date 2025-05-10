be4208125c701cca48c2f414362081fbca338ed8
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2025-05-10 01:26:47 -0300

    Cleaning wlogout setup

--- home/hyprland/wlogout/default.nix
+++ home/hyprland/wlogout/default.nix
@@ -1,12 +1,55 @@
 { pkgs, ... }:
 {
-
-  home.file.".config/wlogout/icons" = {
-    source = ./config/icons;
-  };
-
   programs.wlogout = {
     enable = true;
-    style = ./config/style.css;
+    style = ''
+      * {
+        background-image: none;
+      }
+      window {
+        background-color: rgba(12, 12, 12, 0.6);
+      }
+      button {
+        color: #ffffff;
+        background-color: #1e1e2e;
+        border-style: solid;
+        border-width: 2px;
+        border-radius: 8px;
+        margin: 25px;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: 25%;
+      }
+
+      button:focus,
+      button:active,
+      button:hover {
+        background-color: #313244;
+        outline-style: none;
+      }
+      #lock {
+        background-image: image(url("${pkgs.wlogout}/share/wlogout/icons/lock.png"));
+      }
+
+      #logout {
+        background-image: image(url("${pkgs.wlogout}/share/wlogout/icons/logout.png"));
+      }
+
+      #suspend {
+        background-image: image(url("${pkgs.wlogout}/share/wlogout/icons/suspend.png"));
+      }
+
+      #hibernate {
+        background-image: image(url("${pkgs.wlogout}/share/wlogout/icons/hibernate.png"));
+      }
+
+      #shutdown {
+        background-image: image(url("${pkgs.wlogout}/share/wlogout/icons/shutdown.png"));
+      }
+
+      #reboot {
+        background-image: image(url("${pkgs.wlogout}/share/wlogout/icons/reboot.png"));
+      }
+    '';
   };
 }
